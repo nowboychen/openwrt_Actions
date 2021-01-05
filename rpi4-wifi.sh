dea38d37fb0f9dd51989f6d15176f33835bfb2c3
nowboychen <48378693+nowboychen@users.noreply.github.com>
2021-01-05 20:57:16 +0800

    Update rpi4-wifi.sh

--- rpi4-wifi.sh
+++ rpi4-wifi.sh
@@ -2,8 +2,11 @@
 
 # This script is only for Raspberry Pi 4, Thanks @xmapst
 
-sed -i 's/36/44/g;s/VHT80/VHT20/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/36/44/g;s/VHT80/VHT20/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#1. Modify default IP
-#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+# echo "修改wifi名称"
+# sed -i "s/OpenWrt/TP-LINK_8EE6/g" openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# echo "设置lan ip"
+# sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
