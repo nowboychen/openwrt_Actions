42d6ea8d166dbdc5ef353b811e2c13d99b85cce4
nowboychen <48378693+nowboychen@users.noreply.github.com>
2021-01-05 12:03:55 +0800

    Update rpi4-wifi.sh

--- rpi4-wifi.sh
+++ rpi4-wifi.sh
@@ -4,3 +4,6 @@
 
 sed -i 's/36/44/g;s/VHT80/VHT20/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
