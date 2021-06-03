659c3d20f16ca31b781d2acf734c778cdf5c7256
nowboychen <48378693+nowboychen@users.noreply.github.com>
2021-06-03 19:15:48 +0800

    Add files via upload

---
+++ extend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Adding a plug-in source to feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> openwrt/feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small " >> openwrt/feeds.conf.default
